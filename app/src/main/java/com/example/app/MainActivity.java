90c57da4234c2b586b4415e2e7de0fb7034dd151
Yashwanthmne <yashwanth.mne@gmail.com>
2019-10-17 22:18:38 +0530

    Changed splash screen timeout delays

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -33,14 +40,30 @@ public class MainActivity extends Activity {
         mRefresh.setEnabled(false);
         mSplash.setVisibility(View.VISIBLE);
         mWebView.setVisibility(View.GONE);
-        mWebView.setWebViewClient(new WebViewClient());
-
+//        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                //Required functionality here
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+                mSplash.setVisibility(View.GONE);
+                mRefresh.setRefreshing(true);
+            }
+        };
+        handler.postDelayed(r, 5000);
+
 //         REMOTE RESOURCE
-         mWebView.loadUrl("https://www.rawstory.com");
+        mWebView.loadUrl("https://www.rawstory.com");
 
         mRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -53,17 +76,29 @@ public class MainActivity extends Activity {
             }
         });
 
-
-
         mWebView.setWebViewClient(new WebViewClient() {
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                if(mSplash.getVisibility() != View.VISIBLE) {
-                    mRefresh.setRefreshing(true);
-                }
+
             }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Uri uri = Uri.parse(url);
+                if (uri.getHost() != null && uri.getHost().contains("rawstory.com")) {
+                    if(mSplash.getVisibility() != View.VISIBLE) {
+                        mRefresh.setRefreshing(true);
+                    }
+                    return false;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                view.getContext().startActivity(intent);
+                return true;
+            }
+
 
             public void onPageFinished(WebView view, String url) {
                 Log.e("YM", "INSIDE onPageFinished()");
