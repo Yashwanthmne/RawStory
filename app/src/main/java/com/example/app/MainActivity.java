ffc397d83e4b1d2f35b7e64610fb9ce407128a1c
Yashwanthmne <yashwanth.mne@gmail.com>
2019-12-14 11:52:58 +0530

    Added bottom nav bar with customisable links

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -20,12 +20,16 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
 
+import com.eightbitlab.bottomnavigationbar.BottomBarItem;
+import com.eightbitlab.bottomnavigationbar.BottomNavigationBar;
+
 public class MainActivity extends Activity {
 
     private WebView mWebView;
     private ImageView mSplash;
     private SwipeRefreshLayout mRefresh;
     private ViewTreeObserver.OnScrollChangedListener mOnScrollChangedListener;
+    BottomNavigationBar bottomMenu;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -35,6 +39,21 @@ public class MainActivity extends Activity {
         mWebView = findViewById(R.id.activity_main_webview);
         mSplash = findViewById(R.id.splash);
         mRefresh = findViewById(R.id.swiperefresh);
+        bottomMenu = findViewById(R.id.nav_view);
+
+
+
+        BottomBarItem latest = new BottomBarItem(R.drawable.ic_trending_filled, R.string.latest);
+        BottomBarItem trending = new BottomBarItem(R.drawable.ic_trending, R.string.trending);
+        BottomBarItem exclusive = new BottomBarItem(R.drawable.ic_exclusive, R.string.exclusive);
+        BottomBarItem video = new BottomBarItem(R.drawable.ic_youtube, R.string.video);
+        BottomBarItem contactus = new BottomBarItem(R.drawable.ic_menu, R.string.contactus);
+
+        bottomMenu.addTab(latest);
+        bottomMenu.addTab(trending);
+        bottomMenu.addTab(exclusive);
+        bottomMenu.addTab(video);
+        bottomMenu.addTab(contactus);
 
 
         mRefresh.setEnabled(false);
@@ -64,6 +83,46 @@ public class MainActivity extends Activity {
 
 //         REMOTE RESOURCE
         mWebView.loadUrl("https://www.rawstory.com");
+
+        bottomMenu.setOnSelectListener(new BottomNavigationBar.OnSelectListener() {
+            /**
+             * Change URL below to the suited webpage to be loaded. This is
+             * an extremley easily plug and play system, it will not break anything.
+             * always include the entire URL as in mWebView.loadUrl("https://www.rawstory.com")
+             * and NOT mWebView.loadUrl("www.rawstory.com")
+             * @param position
+             */
+            @Override
+            public void onSelect(int position) {
+                if(position == 0){
+                    mWebView.loadUrl("https://www.rawstory.com");
+                    mRefresh.setRefreshing(true);
+                }
+                if(position == 1){
+
+                    mWebView.loadUrl("https://www.rawstory.com/category/breaking-banner/");
+                    mRefresh.setRefreshing(true);
+
+                }
+                if(position == 2){
+
+                    mWebView.loadUrl("https://www.rawstory.com/raw-story-investigate/");
+                    mRefresh.setRefreshing(true);
+                }
+                if(position == 3){
+
+                    mWebView.loadUrl("https://www.rawstory.com/category/all-video/");
+                    mRefresh.setRefreshing(true);
+
+                }
+                if(position == 4){
+                    mWebView.loadUrl("https://www.rawstory.com/category/all-video/");
+                    mRefresh.setRefreshing(true);
+
+                }
+
+            }
+        });
 
         mRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
